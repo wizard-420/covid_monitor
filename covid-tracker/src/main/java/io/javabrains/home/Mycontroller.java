5478323f2a1b5a1dc4a75bbe64856ecf48267634
wizard-420 <stringroyale@gmail.com>
2021-04-05 12:33:13 +0530

    final push

--- covid-tracker/src/main/java/io/javabrains/home/Mycontroller.java
+++ covid-tracker/src/main/java/io/javabrains/home/Mycontroller.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import io.javabrains.CoronaVirusDataService;
+import io.javabrains.models.DeathStats;
 import io.javabrains.models.LocationStats;
+import io.javabrains.models.RecoveryStats;
 
 @Controller
 public class Mycontroller {
@@ -25,6 +27,28 @@ public class Mycontroller {
 		model.addAttribute("totalReportedCases", totalReportedCases);
 		model.addAttribute("toalNewCases", toalNewCases );
 		return "home";
+	}
+	@GetMapping("/deaths")
+	public String death(Model model) {
+		List<DeathStats> allstats = coronaVirusDataService.getDeathstat();
+		int totalReportedCases = allstats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int toalNewCases = allstats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+		model.addAttribute("dstat", allstats );
+		model.addAttribute("dtotalReportedCases", totalReportedCases);
+		model.addAttribute("dtoalNewCases", toalNewCases );
+		return "death";
+		
+	}
+	@GetMapping("/recovery")
+	public String recovery(Model model) {
+		List<RecoveryStats> allstats = coronaVirusDataService.getRecstats();
+		int totalReportedCases = allstats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int toalNewCases = allstats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+		model.addAttribute("rstat", allstats );
+		model.addAttribute("rtotalReportedCases", totalReportedCases);
+		model.addAttribute("rtoalNewCases", toalNewCases );
+		return "recovery";
+		
 	}
 
 }
